bfe81cfbd7a306cc3ff108a888b3f48d9c4df05a
SamuelMakoholi <samuelmakoh@gmail.com>
2023-06-07 11:06:43 +0200

    Third commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Zimbawe Teachers Leave and Transfer Management System</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Zimbabwe Teachers Leave and Transfer Management System</h1>
                                     </div>
                                     <form class="user" method="POST" action="{{ route('login') }}">
                                     @csrf
